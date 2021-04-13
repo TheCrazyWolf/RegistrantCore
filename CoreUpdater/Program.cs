03850839dd9f9e06d05d39ca48e8ead79e625ae8
TheCrazyWolf <tcrazywolf63@gmail.com>
2021-04-13 20:17:10 +0400

    Наработки автоапдйетера

---
+++ CoreUpdater/Program.cs
@@ -0,0 +1,25 @@
+﻿using LibGit2Sharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace CoreUpdater
+{
+    class Program
+    {
+        private string _actualVer { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registrant Core Updater");
+            Console.WriteLine("Checking for updates");
+        }
+
+        public void CheckVersion()
+        {
+            WebClient web = new WebClient();//Для использования WebClient подключи System.Net 
+            _actualVer = web.DownloadString("Адрес к txt файлу");
+        }
+    }
+}
