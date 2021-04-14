467ae606dd92720f3723b37c82aed8d940078e72
TheCrazyWolf <tcrazywolf63@gmail.com>
2021-04-14 22:30:07 +0400

    фикс

--- Registrant/Settings/App.Designer.cs
+++ Registrant/Settings/App.Designer.cs
@@ -25,7 +25,7 @@ namespace Registrant.Settings {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.3")]
         public string AppVersion {
             get {
                 return ((string)(this["AppVersion"]));
