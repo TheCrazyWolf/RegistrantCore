0fff6b8be1fa9e67d7ecae99ef92d98a03cdeaf9
TheCrazyWolf <tcrazywolf63@gmail.com>
2021-04-14 19:55:03 +0400

    HOT FIX с доменкой

--- Registrant/DB/RegistrantCoreContext.cs
+++ Registrant/DB/RegistrantCoreContext.cs
@@ -29,7 +29,7 @@ namespace Registrant.DB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Server=" + Settings.Connection.Default.IP + "," + Settings.Connection.Default.Port + ";" + "Database=" + Settings.Connection.Default.Database + ";User ID=" + Settings.Connection.Default.Login + ";" + "Password=" + Settings.Connection.Default.Password + ";Trusted_Connection=True;MultipleActiveResultSets=True;");
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Server=" + Settings.Connection.Default.IP + "," + Settings.Connection.Default.Port + ";" + "Database=" + Settings.Connection.Default.Database + ";User ID=" + Settings.Connection.Default.Login + ";" + "Password=" + Settings.Connection.Default.Password + ";MultipleActiveResultSets=True;");
             }
         }
 
